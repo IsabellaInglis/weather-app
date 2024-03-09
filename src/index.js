eac89c4cd5adac79ab851faf66db655e21324961
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-03-09 18:58:56 +0000

    Integrated API for city and temp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function searchCity(event) {
+function refreshWeather(response) {
+  let currentTemp = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  currentTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  // make api call and update the interface
+  let apiKey = "dc6760cf7088c245e5a42a646bco203t";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
+function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Paris");
