acf66cb681f7523b8e5933f93f6175609fde9b91
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-03-10 14:02:38 +0000

    Made weather icon to change depending on weather

--- src/index.js
+++ src/index.js
@@ -31,7 +31,12 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"
+      class="temp-icon"
+    />`;
   cityElement.innerHTML = response.data.city;
   currentTemp.innerHTML = Math.round(temperature);
   weatherDescriptionElement.innerHTML = response.data.condition.description;
